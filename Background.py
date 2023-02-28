fff6af38c9d88d9cbc315a97669a772c9acb13db
Alma Hagstrom <hagstrom.alma@gmail.com>
2023-02-28 12:00:21 -0500

    sdfsf

---
+++ Background.py
@@ -0,0 +1,29 @@
+import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Set the dimensions of the screen
+screen_width = 640
+screen_height = 480
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Set the background color to white
+background_color = (255, 255, 255)
+
+# Fill the screen with the background color
+screen.fill(background_color)
+
+# Update the screen
+pygame.display.flip()
+
+# Run the game loop
+running = True
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# Quit pygame
+pygame.quit()
